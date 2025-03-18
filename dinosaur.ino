8fe1e6344da9aa3a0191db98e30b072a085dfc9e
unknxwnwxrld <166748477+unknxwnwxrld@users.noreply.github.com>
2025-03-19 00:25:37 +0100

    Update dinosaur.ino
    Added custom characters

--- dinosaur.ino
+++ dinosaur.ino
@@ -2,7 +2,6 @@
 #include <LiquidCrystal_I2C.h>
 
 #define JUMPBUTTON_PIN 5
-#define tickValue 200
 
 #define enemyHorStartPos 15
 #define enemyVertStartPos 1
@@ -19,15 +18,80 @@ int randomDistance = 5;
 int randomDistance2 = 5;
 int gameStartTimer = 3;
 
+int tickValue = 200;
+
 int enemyHorPos = enemyHorStartPos;
 int enemyVertPos = enemyVertStartPos;
 int playerHorPos = playerHorStartPos;
 int playerVertPos = playerVertStartPos;
 
-const char enemyChar = 'Q';
-const char playerChar = 'P';
-
 bool enemyTouch = false;
+
+byte playerChar[] = {
+  B11110,
+  B01110,
+  B11110,
+  B01100,
+  B11111,
+  B01100,
+  B11110,
+  B01010
+};
+
+byte enemyChar[] = {
+  B00100,
+  B10100,
+  B10101,
+  B10101,
+  B11111,
+  B01110,
+  B00110,
+  B00110
+};
+
+byte birdChar[] = {
+  B00010,
+  B00110,
+  B01100,
+  B11110,
+  B11110,
+  B00000,
+  B00000,
+  B00000
+};
+
+byte hearthChar1[] = {
+  B00000,
+  B00110,
+  B01111,
+  B01110,
+  B01111,
+  B00110,
+  B00011,
+  B00001
+};
+
+byte hearthChar2[] = {
+  B00000,
+  B01100,
+  B11110,
+  B01110,
+  B11110,
+  B01100,
+  B11000,
+  B10000
+};
+
+byte hearthChar3[] = {
+  B00000,
+  B00000,
+  B11011,
+  B11111,
+  B11111,
+  B01110,
+  B00100,
+  B00000
+};
 
 void setup() {
   pinMode(JUMPBUTTON_PIN, INPUT_PULLUP);
@@ -35,15 +99,25 @@ void setup() {
   lcd.init();
   lcd.begin(16, 2);
   lcd.setBacklight(1);
-  // startMenu();
 
-  // Для отладки
+  lcd.createChar(0, playerChar);
+  lcd.createChar(1, enemyChar);
+  lcd.createChar(2, birdChar);
+  lcd.createChar(3, hearthChar1);
+  lcd.createChar(4, hearthChar2);
+  lcd.createChar(5, hearthChar3);
+
+
   Serial.begin(9600);
 }
 
 void loop() {
   randomDistance = random(3, 7);
   randomDistance2 = random(1, 3);
+
+  lcd.setCursor(0, 0);
+  lcd.print("\"Ardosaur\"");
+
   if(millis() - timeStamp > tickValue * 5) {
     timeStamp = millis();
     lcd.clear();
@@ -92,8 +166,18 @@ void mainGame() {
   }
 
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("YOU DIE!");
+  lcd.setCursor(8, 0);
+  lcd.write(byte(5));
+  delay(700);
+  lcd.setBacklight(0);
+  delay(300);
+  lcd.clear();
+  lcd.setBacklight(1);
+
+  lcd.setCursor(7, 0);
+  lcd.write(byte(3));
+  lcd.setCursor(9, 0);
+  lcd.write(byte(4));
 
   while(digitalRead(JUMPBUTTON_PIN) == HIGH) {
     Serial.println("Waiting for button signal");
@@ -107,26 +191,19 @@ void mainGame() {
 
 void drawEnemy() {
   // Стирание предидущей позиции врага и отрисовка новой
-  
+  lcd.setCursor(enemyHorPos, enemyVertPos);
+  lcd.write(byte(1));
+  lcd.setCursor(enemyHorPos + randomDistance2, enemyVertPos);
+  lcd.write(byte(1));
+  lcd.setCursor(enemyHorPos + randomDistance, enemyVertPos - 1);
+  lcd.write(byte(2));
 
   lcd.setCursor(enemyHorPos + 1, enemyVertPos);
   lcd.print(" ");
-  lcd.setCursor(enemyHorPos + 1 + randomDistance, enemyVertPos - 1);
-  lcd.print(" ");
   lcd.setCursor(enemyHorPos + 1 + randomDistance2, enemyVertPos);
   lcd.print(" ");
-
-  // lcd.setCursor(0, 1);
-  // lcd.print(" ");
-  // lcd.setCursor(0, 0);
-  // lcd.print(" ");
-
-  lcd.setCursor(enemyHorPos, enemyVertPos);
-  lcd.print(enemyChar);
-  lcd.setCursor(enemyHorPos + randomDistance, enemyVertPos - 1);
-  lcd.print(enemyChar);
-  lcd.setCursor(enemyHorPos + randomDistance2, enemyVertPos);
-  lcd.print(enemyChar);
+  lcd.setCursor(enemyHorPos + 1 + randomDistance, enemyVertPos - 1);
+  lcd.print(" ");
 
   enemyHorPos--;
 
@@ -139,7 +216,7 @@ void drawEnemy() {
 
 void drawPlayer() {
   lcd.setCursor(playerHorPos + 1, playerVertPos);
-  lcd.print(playerChar);
+  lcd.write(byte(0));
 
   // Стирание игрока по вертикали
   lcd.setCursor(playerHorPos + 1, playerVertPos - 1);
